e9dfa9e01a35a7ef7fc8be3308278831811a15b9
Cosimo <cor94@live.it>
2016-07-02 12:25:25 +0200

    info from server

--- app/src/main/java/gruppo4/dib/sms2016/mydib2016/business/not_logged/InformazioniUni.java
+++ app/src/main/java/gruppo4/dib/sms2016/mydib2016/business/not_logged/InformazioniUni.java
@@ -1,29 +1,124 @@
- package gruppo4.dib.sms2016.mydib2016.business.not_logged;
+package gruppo4.dib.sms2016.mydib2016.business.not_logged;
 
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
 
 import gruppo4.dib.sms2016.mydib2016.R;
+import gruppo4.dib.sms2016.mydib2016.network.Network;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class InformazioniUni extends Fragment {
+
+    private Intent emailIntent;
+    TextView doveSiamoView, pecView, nomeDiretView, emailDiretView, nomeSegretView, emailSegretView;
+
+    RequestQueue queue;
+    ProgressDialog progressDialog;
+
 
     public InformazioniUni() {
         // Required empty public constructor
     }
 
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        queue = Network.getInstance(getActivity()).
+                getRequestQueue();
+        progressDialog = new ProgressDialog(getActivity());
+
+        emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+        emailIntent.setType("plain/text");
+
+        doveSiamoView = (TextView) getActivity().findViewById(R.id.textDoveSiamo);
+        pecView = (TextView) getActivity().findViewById(R.id.textPEC);
+        nomeDiretView = (TextView) getActivity().findViewById(R.id.nomeDirettore);
+        emailDiretView = (TextView) getActivity().findViewById(R.id.emailDirettore);
+        nomeSegretView = (TextView) getActivity().findViewById(R.id.nomeSegretario);
+        emailSegretView = (TextView) getActivity().findViewById(R.id.emailSegretario);
+
+        setUI("http://mydib2016.altervista.org/api/index.php/infouni");
+
+                /*
+                emailDiretView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{emailDiretView.getText().toString()});
+                        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "INFORMAZIONI");
+                        emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "");
+                        startActivity(Intent.createChooser(emailIntent, "Email al direttore."));
+                    }
+                });
+
+        emailSegretView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{emailSegretView.getText().toString()});
+                emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "INFORMAZIONI");
+                emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "");
+                startActivity(Intent.createChooser(emailIntent, "Email al segretario amministrativo."));
+            }
+        });
+*/
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info_uni, container, false);
+        View rootview = inflater.inflate(R.layout.fragment_info_uni, container, false);
+
+        return rootview;
+    }
+
+    public void setUI(String url) {
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            doveSiamoView.setText(response.getString("doveSiamo"));
+                            pecView.setText(response.getString("pec"));
+                            nomeDiretView.setText(response.getString("nomeDir"));
+                            emailDiretView.setText(response.getString("emailDir"));
+                            nomeSegretView.setText(response.getString("nomeSegr"));
+                            emailSegretView.setText(response.getString("emailSegr"));
+                        } catch(Exception e) {
+                            e.printStackTrace();
+                        }
+                        progressDialog.dismiss();
+                    }
+
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println("JSON Errore connessione info: " + error.networkResponse);
+                        progressDialog.dismiss();
+                    }
+                });
+        Network.getInstance(getActivity()).addToRequestQueue(jsObjRequest);
+        progressDialog.setTitle("Attendere");
+        progressDialog.setMessage("Caricamento info");
+        progressDialog.show();
     }
 
 }
